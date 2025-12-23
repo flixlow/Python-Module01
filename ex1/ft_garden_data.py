54a308bc640eac20282af58545dc644e9a0f1135
flixlow <felix.lauwerie@gmail.com>
2025-12-23 18:11:10 +0100

    class usage and uderstanding

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,14 +1,28 @@
+#!/usr/bin/env python3
+
 class Plant:
-    def __init__(self, name, age, height):
-        self.name = name
-        self.age = age
-        self.height = height
-    
-    def test(self):
-        print(self.name)
+    def __init__(self, name, height, age):
+        self.p_name = name
+        self.p_height = height
+        self.p_age = age
+
+    def get_info(self):
+        print(f"{self.p_name}: {self.p_height}cm, {self.p_age} days old")
+
 
 def ft_garden_data():
-    plant1 = Plant("pink", 22, 5)
-    plant1.test()
+    print("=== Garden Plant Registry ===")
+    plant1 = Plant("Rose", 25, 30)
+    plant2 = Plant("sunflower", 80, 45)
+    plant3 = Plant("cactus", 15, 120)
+    plant1.get_info()
+    plant2.get_info()
+    plant3.get_info()
 
-ft_garden_data()
+
+def main():
+    ft_garden_data()
+
+
+if __name__ == "__main__":
+    main()
