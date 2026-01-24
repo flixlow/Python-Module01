15baf5c78ade9ea285ea9b2095f46fcdd66b3f9e
Felix Lauwerie <flauweri@z1r2p1.42lyon.fr>
2026-01-24 12:26:24 +0100

    plant types

--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -3,29 +3,45 @@
 class GardenManager:
     """Global garden manager"""
     def __init__(self):
-        self.Garden = []
+        self.garden: list[Garden] = []
+        self.garden_index: int = 0
 
-    class Garden_stats:
+    class GardenStats:
+        def __init__(self):
+            pass
 
-    def create_garden_network(self):
+    @classmethod
+    def create_garden_network(cls, gardens):
+        gam = cls()
+        gam.garden = gardens
+        return gam
 
 
-class Garden(GardenManager):
+class Garden:
     def __init__(self, owner_name):
-        super().__init()
+        pass
 
 
-class Plant(Garden):
+class Plant:
     """parent class for plant"""
-    def __init__(self):
+    def __init__(self, name: str, height: int, age: int):
+        self.name: str = name
+        self.height: int = height
+        self.age: int = age
 
 
 class FloweringPlant(Plant):
+    pass
+
+
 class PrizeFlower(FloweringPlant):
+    pass
 
 
 def main():
     print("=== Garden Management System Demo ===")
+    gam = GardenManager.create_garden_network(["bob", "alice"])
+    print(gam.garden)
 
 
 if __name__ == "__main__":
