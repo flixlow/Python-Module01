de3d1fcc577888cb2e2fe26fa5cc6d8f49a65ba6
Felix Lauwerie <flauweri@z1r2p2.42lyon.fr>
2026-01-25 20:52:06 +0100

    pre-correction

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -6,9 +6,9 @@ class Plant:
         self.height = height
         self.age = age
 
-    def get_info(self) -> None:
-        print(f"{self.name} ({self.__class__.__name__}): {self.height}cm, \
-{self.age} days, ", end="")
+    def get_info(self) -> str:
+        return f"{self.name} ({self.__class__.__name__}):\
+ {self.height}cm, {self.age} days, "
 
 
 class Tree(Plant):
@@ -20,12 +20,14 @@ class Tree(Plant):
 
     def produce_shade(self) -> None:
         pi: float = 3.14
-        height = self.height
-        self.shade = round(pi * height * height)
+        self.shade = round(pi * (self.height / 100) ** 2)
 
-    def get_info(self) -> None:
-        super().get_info(), print(f"{self.trunk_diameter}cm diameter")
-        print(f"{self.name} provides {self.shade} square meters of shade\n")
+    def get_info(self) -> str:
+        return (
+            f"{super().get_info()}"
+            f"{self.trunk_diameter}cm diameter "
+            f"\n{self.name} provides {self.shade} square meters of shade\n"
+        )
 
 
 class Flower(Plant):
@@ -37,12 +39,13 @@ class Flower(Plant):
     def bloom(self) -> None:
         self.bloom_status = True
 
-    def get_info(self) -> None:
-        super().get_info(), print(f"{self.color} color")
-        if self.bloom_status is True:
-            print(f"{self.name} is blooming beautifully!\n")
+    def get_info(self) -> str:
+        info = f"{super().get_info()}{self.color} color "
+        if self.bloom_status:
+            info += f"\n{self.name} is blooming beautifully!\n"
         else:
-            print(f"{self.name} is waiting to bloom...\n")
+            info += f"\n{self.name} is waiting to bloom...\n"
+        return info
 
 
 class Vegetable(Plant):
@@ -52,27 +55,30 @@ class Vegetable(Plant):
         self.harvest_season: str = season
         self.nutritional_value: str = value
 
-    def get_info(self) -> None:
-        super().get_info(), print(f"{self.harvest_season} harvest")
-        print(f"{self.name} is rich in vitamin {self.nutritional_value}\n")
+    def get_info(self) -> str:
+        return (
+            f"{super().get_info()}"
+            f"{self.harvest_season} harvest "
+            f"\n{self.name} is rich in {self.nutritional_value}\n"
+        )
 
 
 def main() -> None:
     print("=== Garden Plant Types ===")
     flower1: Flower = Flower("Rose", 25, 30, "red")
     flower2: Flower = Flower("Sunflower", 120, 50, "yellow")
-    flower1.get_info()
-    flower2.get_info()
+    print(flower1.get_info())
+    print(flower2.get_info())
     tree1: Tree = Tree("Oak", 500, 1825, 50)
     tree2: Tree = Tree("Birch", 450, 160, 50)
-    tree1.get_info()
-    tree2.get_info()
-    vegetable1: Vegetable = Vegetable("Tomato", 80, 90, "summer", "C")
-    vegetable2: Vegetable = Vegetable("Carrot", 70, 90, "automn", "C")
-    vegetable1.get_info()
-    vegetable2.get_info()
+    print(tree1.get_info())
+    print(tree2.get_info())
+    vegetable1: Vegetable = Vegetable("Tomato", 80, 70, "summer", "vitamin C")
+    vegetable2: Vegetable = Vegetable("Carrot", 20, 90, "autumn", "vitamin B")
+    print(vegetable1.get_info())
+    print(vegetable2.get_info())
     flower1.bloom()
-    flower1.get_info()
+    print(flower1.get_info())
 
 
 if __name__ == "__main__":
